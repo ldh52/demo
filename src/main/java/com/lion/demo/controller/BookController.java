a5909393e9e879928641b943c8d88510b9d141b4
ldh52 <yahopet79@gmail.com>
2024-12-03 22:10:22 +0900

    Order

--- src/main/java/com/lion/demo/controller/BookController.java
+++ src/main/java/com/lion/demo/controller/BookController.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/book")
