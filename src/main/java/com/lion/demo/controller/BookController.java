83f90695e0cde2a7041efd719b6b947a5491279e
ldh52 <yahopet79@gmail.com>
2024-12-03 13:42:01 +0900

    Table

---
+++ src/main/java/com/lion/demo/controller/BookController.java
@@ -0,0 +1,34 @@
+package com.lion.demo.controller;
+
+import com.lion.demo.entity.Book;
+import com.lion.demo.service.BookService;
+import com.lion.demo.service.CsvFileReaderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    @Autowired private CsvFileReaderService csvFileReaderService;
+    @Autowired private BookService bookService;
+
+    @GetMapping("/list")
+    public String list(@RequestParam(name="p", defaultValue = "1") int page,
+        Model model) {
+        List<Book> bookList = bookService.getBooksByPage(page);
+        model.addAttribute("bookList", bookList);
+        return "book/list";
+    }
+
+    @GetMapping("/yes24")
+    public String yes24() {
+        csvFileReaderService.csvFileToH2();
+        return "redirect:/book/list";
+    }
+}
---
+++ src/main/java/com/lion/demo/entity/Book.java
@@ -0,0 +1,35 @@
+package com.lion.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bid;       // primary key, auto_increment
+
+    private String title;
+    private String author;
+    private String company;
+    private int price;
+    private String imageUrl;
+
+    @Column(length = 8191)
+    private String summary;     // column 길이: 255 -> 8191
+}
