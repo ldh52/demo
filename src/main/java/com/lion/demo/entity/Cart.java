f9534678b34db9d5a6d62cf611354f2764a8ab9f
ldh52 <yahopet79@gmail.com>
2024-12-03 14:51:53 +0900

    Mall

---
+++ src/main/java/com/lion/demo/entity/Cart.java
@@ -0,0 +1,36 @@
+package com.lion.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long cid;
+
+    @ManyToOne
+    @JoinColumn(name = "uid")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "bid")
+    private Book book;
+
+    private int quantity;
+}
---
+++ src/main/java/com/lion/demo/repository/CartRepository.java
@@ -0,0 +1,9 @@
+package com.lion.demo.repository;
+
+import com.lion.demo.entity.Cart;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long>{
+    List<Cart> findByUserUid(String userUid);
+}
